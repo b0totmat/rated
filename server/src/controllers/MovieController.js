e3546e2e236259e53263ae46102daa7e8353ed5f
b0totmat <t0thm4t3@gmail.com>
2025-02-05 17:51:00 +0100

    MovieController - functions for API endpoints

---
+++ server/src/controllers/MovieController.js
@@ -0,0 +1,69 @@
+import { Movie } from "../models"
+
+async function getAll(req, res) {
+    try {
+        const movies = await Movie.findAll()
+        res.status(200).json(movies)
+    } catch(e) {
+        res.status(500)
+    }
+}
+
+async function getById(req, res) {
+    const id = req.params.id
+    
+    try {
+        const movie = await Movie.findByPk(id)
+
+        if(!movie) {
+            res.status(404).json({ message: 'Movie not found.' })
+        }
+
+        res.status(200).json(movie)
+    } catch(e) {
+        res.status(500)
+    }
+}
+
+async function insert(req, res) {
+    try {
+        const newMovie = await Movie.create(req.body)
+        res.status(201).json(newMovie)
+    } catch(e) {
+        res.status(500)
+    }
+}
+
+async function update(req, res) {
+    const id = req.params.id
+
+    try {
+        const movieToUpdate = Movie.findByPk(id)
+
+        if(!movieToUpdate) {
+            res.status(404).json({ message: 'Movie not found.' })
+        }
+
+        await movieToUpdate.update(req.body)
+    } catch(e) {
+        res.status(500)
+    }
+}
+
+async function remove(req, res) {
+    const id = req.params.id
+
+    try {
+        const movieToDelete = Movie.findByPk(id)
+
+        if(!movieToDelete) {
+            res.status(404).json({message: 'Movie not found.'})
+        }
+
+        await movieToDelete.destroy()
+    } catch(e) {
+        res.status(500)
+    }
+}
+
+export default { getAll, getById, insert, update, remove }
