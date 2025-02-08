bf156f3efa208b50d70916ea0d203e72322d5332
b0totmat <t0thm4t3@gmail.com>
2025-02-08 12:03:31 +0100

    Designing MoviesView

--- client/src/views/MoviesView.vue
+++ client/src/views/MoviesView.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Movies</h1>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-3 card">
+        <div class="card-body">
+          <h5 class="card-title">Title</h5>
+          <p class="card-text">Rating:</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+</script>
