e7dd9a14940c7430cb3ac01da1f47ef1301f1ff0
b0totmat <t0thm4t3@gmail.com>
2025-02-08 16:56:30 +0100

    Redirect from new rating page after logging out

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView, useRoute, useRouter } from 'vue-router'
 import { useTokenStore } from './stores/tokens'
 
 import Header from './components/Header.vue'
+
+const route = useRoute(),
+      router = useRouter()
 
 const tokenStore = useTokenStore()
 
 const logout = () => {
   tokenStore.setToken(null)
   tokenStore.setUser(null)
+
+  if(route.name === 'new-rating') {
+    router.push('/')
+  }
 }
 </script>
 
